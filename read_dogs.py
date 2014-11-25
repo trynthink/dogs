4cee7df0b0aca95afe74c1dc69e7a298a9644839
Chioke Harris <chioke.harris@gmail.com>
2014-11-25 13:08:40 -0500

    Uncommented JSON file read lines
    Uncommented the lines of code already present that enable the importation of data from a JSON file and conversion of the associated data into instances of the Dog class.

--- read_dogs.py
+++ read_dogs.py
@@ -16,51 +16,23 @@ class Dog:
 
 # WHEN IMPORTING, WHAT ABOUT ERRORS FROM INCOMPLETE OBJECTS (ENTRIES)?
 
-# def dog_unserialize(q):
-#     # strip off name with pop
-#     # name = q.pop('Name') # no default given, will throw KeyError if no name is provided in the object definition
-#     cls = dog
-#     obj = cls.__new__(cls) # new instance
-#     for key, value in q.items():
-#         setattr(obj, key, value)
-#         return obj
+# Define what to do with the objects in the JSON file
+def dog_unserialize(q):
+    # strip off name with pop
+    # name = q.pop('Name') # no default given, will throw KeyError if no name is provided in the object definition
+    cls = Dog
+    obj = cls.__new__(cls) # new instance
+    for key, value in q.items():
+        setattr(obj, key, value)
+    return obj
 
-# Define exceptions for Dog objects
-class DogError(Exception): pass
-class NotStringError(DogError): pass
-class InvalidAgeError(DogError): pass
-class NotNumericError(DogError): pass
-class InvalidColorError(DogError): pass
+# Open database of measures
+jsfl = open('dogs.json')
 
-# Define Dog object validation functions
-def check_dog_name(dog):
-	""" Check that an acceptable name is given to a dog """
-	if not isinstance(dog["Name"], str):
-		raise NotStringError, "Dog name entered is not a string"
+# Load data from database
+data = json.load(jsfl, object_hook=dog_unserialize) # object_hook returns dict, object_pairs_hook returns list of tuples
 
-def check_dog_breed(dog):
-	""" Check that an acceptable breed is given for a dog """
-	is not isinstance(dog["Breed"], str):
-		raise NotStringError, "Dog breed entered is not a string"
-
-def check_dog_color(dog):
-	""" Check that an acceptable color is provided for a dog """
-	colors = ["White", "Black", "Brown", "Sable", "Gray", "Fawn", "Cream"]
-	
-	if not isinstance(dog["Color"], str):
-	for color in dog["Color"]:
-		# STOPPED HERE
-
-def check_dog_age(dog):
-	pass
-
-
-
-# jsfl = open('dogs.json') # open database of measures
-
-# data = json.load(jsfl, object_hook=dog_unserialize) # object_hook returns dict, object_pairs_hook returns list of tuples
-
-# jsfl.close() # close JSON file
+jsfl.close() # close JSON file
 
 # Validate imported dogs
 
