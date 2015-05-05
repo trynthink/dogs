0719393fe7dee6dec261d39d58a91a8a2ae722b0
Chioke Harris <trynthink@gmail.com>
2015-05-05 17:10:10 -0400

    Revised website and JavaScript for form recall
    To test the use of the jQuery function .getJSON(), a form with styled radio buttons and a multiple selection list was added to the HTML, along with a field to be updated with the information from the JSON database. The script was updated with jQuery functions to capture the inputs to the fields on the website and pull the appropriate data from the JSON database.

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 $(document).ready(function(){
-	$.getJSON('resources/dogs.json', function(data){
-		for (var i = 0 ; i < data.length ; i++) {
-			var stuff_to_show = data[i]['Name'];
-			$('#loc').append('<p>' + stuff_to_show + '</p>');
-		}
+
+	// Declare variables for JSON database keys
+	var theBreed;
+	var theStates;
+
+	// Declare variable for sum of entries
+	var total = 0;
+
+	// Display the total in the appropriate location on the page
+	$('#number').text(total)
+
+	// Detect selected radio button - Breed
+	$('#dog_breed').on('change', function(){
+		theBreed = $('input[name=options]:checked', '#dog_breed').val();
 	});
+
+	// Detect selections in list - State(s)
+	$('#US_states').on('click', function (){
+		theStates = $('#US_states').val();
+	});
+
+	// NEED TO PUT IN PLACE SOME ERROR HANDLING ON THE BUTTON CLICK
+	// IF NOT ALL THE SELECTIONS HAVE BEEN MADE
+
+	// Query database and update total when button is clicked
+	$('#update').on('click', function(){
+		// Reset total
+		total = 0;
+		
+		// Sum the totals for all states selected
+		$.getJSON('resources/grouped_dogs.json', function(data){
+			for (var i = 0 ; i < theStates.length ; i++) {
+				total = total + data[theBreed][theStates[i]]['Age'];
+			}
+			
+			// Update total number displayed
+			$('#number').text(total)
+		});
+	});
+
 });
